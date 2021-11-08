c2923c63caf898b65e9eba01b426638dc5d41cc1
Roman Bachurin <Roman.Bachurin@waveaccess.ru>
2021-11-09 00:58:23 +0500

    Refactor QuotesServiceTests, add mapper test

--- RandomQuotes.CoreTests/QuoteServiceTests.cs
+++ RandomQuotes.CoreTests/QuoteServiceTests.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Xunit;
 using Moq;
 using AutoFixture;
 using AutoMapper;
+using RandomQuotes.Abstractions.Models;
+using RandomQuotes.Abstractions.Repositories;
 using RandomQuotes.Core;
 using RandomQuotes.Core.Services;
-using RandomQuotes.DataAccess.Interfaces;
+using RandomQuotes.Resources;
 
 namespace RandomQuotes.CoreTests
 {
@@ -25,16 +26,21 @@ namespace RandomQuotes.CoreTests
         }
 
         [Fact]
+        public void CoreMappingProfileTest()
+        {
+            _mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Fact]
         public async Task GetRandomQuote_Successful()
         {
             // arrange
             var quotesRepositoryMock = new Mock<IQuotesRepository>();
-            var quote = _fixture.Create<DataAccess.Models.Quote>();
+            var quote = _fixture.Create<Quote>();
 
-            quotesRepositoryMock.Setup(x => x.GetRandomQuote())
-                .ReturnsAsync(quote);
+            quotesRepositoryMock.Setup(x => x.GetRandomQuote()).ReturnsAsync(quote);
 
-            var quoteService = new QuotesService(quotesRepositoryMock.Object, _mapper);
+            var quoteService = new QuotesService(quotesRepositoryMock.Object);
 
             // act
             var randomQuoteResponse = await quoteService.GetRandomQuote();
@@ -43,9 +49,31 @@ namespace RandomQuotes.CoreTests
             Assert.NotNull(randomQuoteResponse);
             Assert.True(randomQuoteResponse.IsSuccess);
             Assert.NotNull(randomQuote);
-            Assert.Equal("", randomQuote.Text);
-            Assert.Equal("", randomQuote.Author);
-            Assert.Equal(DateTime.UtcNow, randomQuote.CreatedAtUtc);
+            Assert.Equal(quote.Text, randomQuote.Text);
+            Assert.Equal(quote.Author, randomQuote.Author);
+            Assert.Equal(quote.CreatedAtUtc, randomQuote.CreatedAtUtc);
+        }
+        
+        [Fact]
+        public async Task GetRandomQuote_NoQuotesInDatabase_ReturnsNotFound()
+        {
+            // arrange
+            var quotesRepositoryMock = new Mock<IQuotesRepository>();
+
+            quotesRepositoryMock.Setup(x => x.GetRandomQuote()).ReturnsAsync(default(Quote));
+
+            var quoteService = new QuotesService(quotesRepositoryMock.Object);
+
+            // act
+            var randomQuoteResponse = await quoteService.GetRandomQuote();
+            // assert
+            Assert.NotNull(randomQuoteResponse);
+            Assert.False(randomQuoteResponse.IsSuccess);
+            Assert.Null(randomQuoteResponse.ResultData);
+            Assert.NotNull(randomQuoteResponse.Error);
+            Assert.Equal(DefaultErrorModels.QuotesNotExist.Key, randomQuoteResponse.Error.Key);
+            Assert.Equal(DefaultErrorModels.QuotesNotExist.Kind, randomQuoteResponse.Error.Kind);
+            Assert.Equal(DefaultErrorModels.QuotesNotExist.Message, randomQuoteResponse.Error.Message);
         }
     }
 }
